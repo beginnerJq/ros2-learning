634e380e7376d7fc057f7fb08485e808618ccf90
jiangqi <1292692959@qq.com>
2025-05-08 09:34:44 +0800

    first commit

---
+++ chapt2/ros2_python_node.py
@@ -0,0 +1,13 @@
+import rclpy
+from rclpy.node import Node
+
+def main():
+    rclpy.init() # 初始化工作
+    node = Node('python_node') #创建节点
+    node.get_logger().info('你好 Python 节点！') 
+    node.get_logger().warn('你好 Python 节点！') 
+    rclpy.spin(node) # 运行节点
+    rclpy.shutdown() # 关闭节点
+    
+if __name__ == '__main__':
+    main()
