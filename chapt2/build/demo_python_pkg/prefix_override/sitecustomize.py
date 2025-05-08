66ee010ee1604f41f269e75e235c419d133f5898
jiangqi <1292692959@qq.com>
2025-05-08 18:06:06 +0800

    colcon

--- chapt2/build/demo_python_pkg/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/robot/ros2-learning/chapt2/install/demo_python_pkg'
---
+++ chapt2/chapt_ws/build/demo_python_pkg/colcon_command_prefix_setup_py.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/robot/ros2-learning/chapt2/chapt_ws/install/demo_cpp_pkg/share/demo_cpp_pkg/package.sh"
