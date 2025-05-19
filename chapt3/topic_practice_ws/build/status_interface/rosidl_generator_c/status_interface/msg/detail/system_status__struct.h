9940cf828f030b51f6b597db0cb63344476dc3b3
jiangqi <1292692959@qq.com>
2025-05-19 18:08:30 +0800

    interface

---
+++ chapt3/topic_practice_ws/build/status_interface/rosidl_generator_c/status_interface/msg/detail/system_status__struct.h
@@ -0,0 +1,62 @@
+﻿// NOLINT: This file starts with a BOM since it contain non-ASCII characters
+// generated from rosidl_generator_c/resource/idl__struct.h.em
+// with input from status_interface:msg/SystemStatus.idl
+// generated code does not contain a copyright notice
+
+#ifndef STATUS_INTERFACE__MSG__DETAIL__SYSTEM_STATUS__STRUCT_H_
+#define STATUS_INTERFACE__MSG__DETAIL__SYSTEM_STATUS__STRUCT_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+
+// Constants defined in the message
+
+// Include directives for member types
+// Member 'stamp'
+#include "builtin_interfaces/msg/detail/time__struct.h"
+// Member 'host_name'
+#include "rosidl_runtime_c/string.h"
+
+/// Struct defined in msg/SystemStatus in the package status_interface.
+typedef struct status_interface__msg__SystemStatus
+{
+  /// 记录时间戳
+  builtin_interfaces__msg__Time stamp;
+  /// 主机名
+  rosidl_runtime_c__String host_name;
+  /// CPU使用率
+  float cpu_percent;
+  /// 内存使用率
+  float memory_percent;
+  /// 内存总量
+  float memory_total;
+  /// 内存可用量
+  float memory_avilable;
+  /// 网络发送流量 MB
+  double net_sent;
+  /// 网络接收流量 MB
+  double net_recv;
+} status_interface__msg__SystemStatus;
+
+// Struct for a sequence of status_interface__msg__SystemStatus.
+typedef struct status_interface__msg__SystemStatus__Sequence
+{
+  status_interface__msg__SystemStatus * data;
+  /// The number of valid items in data
+  size_t size;
+  /// The number of allocated items in data
+  size_t capacity;
+} status_interface__msg__SystemStatus__Sequence;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // STATUS_INTERFACE__MSG__DETAIL__SYSTEM_STATUS__STRUCT_H_
